f23b35972af09ad3c874aceebd0667128d4ead06
KARRI-LOKESH <karrilokesh108@gmail.com>
2025-02-10 13:18:29 +0530

    Initial commit for my Django project

--- stdapp/models.py
+++ stdapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
-    name=models.CharField(max_length=20)
-    rollno=models.CharField(max_length=10)
-    age=models.IntegerField()
-    email=models.EmailField(max_length=25)
+    name = models.CharField(max_length=20)
+    rollno = models.CharField(max_length=10, unique=True)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=25)
     phone = models.IntegerField(null=True)
+
+    class Meta:
+        unique_together = ('rollno', 'email')
+
+    def __str__(self):
+        return f"{self.name} ({self.rollno})"
