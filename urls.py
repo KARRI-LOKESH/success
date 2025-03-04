190702b9af17c74432a365183c7eafa99583cfc1
lokesh <karrilokesh108@gmail.com>
2025-03-04 12:29:40 +0530

    email

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from stdapp import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("disp/", views.display),
+    path("student/", include("stdapp.urls")),
+]
